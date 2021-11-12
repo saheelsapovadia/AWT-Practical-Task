939655da6f8116fd4e3e3a94e5aeebf6ae1c848f
saheelsapovadia <59962703+saheelsapovadia@users.noreply.github.com>
2021-11-12 22:45:01 +0530

    kakashi changes added

--- client/src/Home/Home.js
+++ client/src/Home/Home.js
@@ -8,6 +8,7 @@ import {
   Navbar,
   Col,
   Alert,
+  Image,
 } from "react-bootstrap";
 import CardHeader from "react-bootstrap/esm/CardHeader";
 import "./Home.css";
@@ -79,7 +80,8 @@ const Home = () => {
           <Button variant="outline-success">Logout</Button>
         </Container>
       </Navbar>
-      <Container>
+
+      <div className="card-container">
         <Card className="payment-width  card-margin">
           <CardHeader className="payment-header">
             Payment Information
@@ -129,66 +131,121 @@ const Home = () => {
           </Card.Body>
         </Card>
 
-        <Card>
-          <CardHeader>Ph.D. Programme Type</CardHeader>
+        <Card className="phd">
+          <CardHeader>Ph.D Programme Type</CardHeader>
           <Card.Body>
             <Form>
-              <div key={"radio"} className="mb-3">
-                <Form.Check type={"radio"}>
-                  <Form.Check.Input type={"radio"} isValid />
-                  <Form.Check.Label>Full-Time</Form.Check.Label>
-                </Form.Check>
-              </div>
-              <div key={"radio"} className="mb-3">
-                <Form.Check type={"radio"}>
-                  <Form.Check.Input type={"radio"} isValid />
-                  <Form.Check.Label>Part-Time</Form.Check.Label>
-                </Form.Check>
-              </div>
+              <Form.Group as={Row} className="mb-3">
+                <Col sm={10}>
+                  <Form.Check
+                    type="radio"
+                    label="Full-Time"
+                    name="formHorizontalRadios"
+                    id="formHorizontalRadios1"
+                  />
+                  <Form.Check
+                    type="radio"
+                    label="Part-Time"
+                    name="formHorizontalRadios"
+                    id="formHorizontalRadios2"
+                  />
+                </Col>
+              </Form.Group>
             </Form>
           </Card.Body>
         </Card>
 
-        <Card>
-          <CardHeader>Ph.D. Programme</CardHeader>
+        <Card className="phd">
+          <CardHeader>Ph.D Programme</CardHeader>
           <Card.Body>
-            <Form.Select aria-label="Default select example">
-              <option>--Select Faculty for Ph.D Programme--</option>
-              <option value="1">One</option>
-              <option value="2">Two</option>
-              <option value="3">Three</option>
-            </Form.Select>
+            <Form>
+              <Form.Select aria-label="Default select example" type="text">
+                <option>--Select Faculty for Programme--</option>
+                <option value="1">Dr. Ritesh Patel</option>
+                <option value="2">Mr. Ronak Patel</option>
+                <option value="3">Mr. Mugendra Rahevar</option>
+                <option value="4">Mr Martin Parmar</option>
+              </Form.Select>
+            </Form>
           </Card.Body>
         </Card>
 
         <Card>
           <CardHeader>Personal Details</CardHeader>
           <Card.Body>
-            <Alert key={"danger"} variant={"danger"}>
-              NOTE: Use CAPITAL letters
-            </Alert>
-            <Row>
-              <Col>
-                <Form.Group className="mb-3">
-                  <Form.Label>
-                    Name of the Candidate[as in Qualifying degree certificate]
-                  </Form.Label>
-                  <Form.Control />
+            <Alert variant="danger">NOTE: USE CAPITAL LETTERS</Alert>
+            <Form>
+              <Row>
+                <Col>
+                  <Form.Group>
+                    <Form.Label className>
+                      Name of the Candidate[as in Qualifying degree certificate]
+                    </Form.Label>
+                    <Form.Control />
+                  </Form.Group>
+                </Col>
+                <Col>
+                  <Image src="user.png" className="user"></Image>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <Form.Group>
+                    <Form.Label>Gender:</Form.Label>
+                    <Form.Select
+                      aria-label="Default select example"
+                      type="text"
+                    >
+                      <option>--Choose Gender--</option>
+                      <option value="1">Male</option>
+                      <option value="2">Female</option>
+                      <option value="3">Others</option>
+                    </Form.Select>
+                  </Form.Group>
+                </Col>
+                <Col className="upload">
+                  <Button variant="primary">Upload Image</Button>
+                  <span style={{ color: "red" }}>[Upload only JPG/JPEG]</span>
+                </Col>
+              </Row>
+              <hr></hr>
+              <h3>Address Details</h3>
+              <Form.Group className="mb-3" controlId="formGridAddress1">
+                <Form.Label>Address</Form.Label>
+                <Form.Control placeholder="1234 Main St" />
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formGridAddress2">
+                <Form.Label>Address 2</Form.Label>
+                <Form.Control placeholder="Apartment, studio, or floor" />
+              </Form.Group>
+
+              <Row className="mb-3">
+                <Form.Group as={Col} controlId="formGridCity">
+                  <Form.Control placeholder="City" />
                 </Form.Group>
-              </Col>
-              <Col>
-                <Form.Control placeholder="Last name" />
-              </Col>
-            </Row>
+
+                <Form.Group as={Col} controlId="formGridZip">
+                  <Form.Control placeholder="Zip" />
+                </Form.Group>
+              </Row>
+              <Row className="mb-3">
+                <Form.Group as={Col} controlId="formGridState">
+                  <Form.Control placeholder="State" />
+                </Form.Group>
+
+                <Form.Group as={Col} controlId="formGridCountry">
+                  <Form.Control
+                    type="text"
+                    placeholder="India"
+                    readOnly
+                  ></Form.Control>
+                </Form.Group>
+              </Row>
+            </Form>
           </Card.Body>
         </Card>
-      </Container>
-      <Navbar bg="dark" expand="md" variant="dark">
-        <Container fluid>
-          <Navbar.Brand href="#">CHARUSAT - PhD Admission</Navbar.Brand>
-          <Button variant="outline-success">Logout</Button>
-        </Container>
-      </Navbar>
+      </div>
     </>
   );
 };

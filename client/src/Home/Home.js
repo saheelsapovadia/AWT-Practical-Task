c4440abc1d5921028f560f5d14e281bd899781a1
vivekapadia <610vivek@gmail.com>
2021-11-12 22:55:47 +0530

    Home Update2

--- client/src/Home/Home.js
+++ client/src/Home/Home.js
@@ -1,12 +1,18 @@
 import React from "react";
-import { Form, Button, Card, Container, Navbar, Alert,Row,Col } from "react-bootstrap";
+import {
+	Form,
+	Button,
+	Card,
+	Container,
+	Navbar,
+	Alert,
+	Row,
+	Col,
+} from "react-bootstrap";
 import CardHeader from "react-bootstrap/esm/CardHeader";
 import "./Home.css";
 
 const Home = () => {
-	const [value, setValue] = React.useState(2);
-	const handleChange = (val) => setValue(val);
-
 	return (
 		<>
 			<Navbar bg="dark" expand="md" variant="dark">
@@ -26,12 +32,12 @@ const Home = () => {
 								<Form.Control />
 							</Form.Group>
 
-							<Form.Group>
+							<Form.Group className="mb-3">
 								<Form.Label>Transaction Date</Form.Label>
 								<Form.Control type="date" name="date_of_birth"></Form.Control>
 							</Form.Group>
 
-							<Form.Group>
+							<Form.Group className="mb-3">
 								<Form.Label>Amount Rs.</Form.Label>
 								<Form.Control type="text" placeholder="1500" readOnly />
 							</Form.Group>
@@ -50,18 +56,11 @@ const Home = () => {
 					<CardHeader>Ph.D. Programme Type</CardHeader>
 					<Card.Body>
 						<Form>
-							<div key={"radio"} className="mb-3">
-								<Form.Check type={"radio"}>
-									<Form.Check.Input type={"radio"} isValid />
-									<Form.Check.Label>Full-Time</Form.Check.Label>
-								</Form.Check>
-							</div>
-							<div key={"radio"} className="mb-3">
-								<Form.Check type={"radio"}>
-									<Form.Check.Input type={"radio"} isValid />
-									<Form.Check.Label>Part-Time</Form.Check.Label>
-								</Form.Check>
-							</div>
+							<Form.Check.Input type={"radio"} isValid className="mb-3" />
+							<Form.Check.Label>Full-Time</Form.Check.Label>
+							<br />
+							<Form.Check.Input type={"radio"} isValid className="mb-3" />
+							<Form.Check.Label>Part-Time</Form.Check.Label>
 						</Form>
 					</Card.Body>
 				</Card>
@@ -89,17 +88,104 @@ const Home = () => {
 								<Form.Group className="mb-3">
 									<Form.Label>
 										Name of the Candidate[as in Qualifying degree certificate]
+										<span style={{ color: "red" }}>*</span>
 									</Form.Label>
 									<Form.Control />
 								</Form.Group>
 							</Col>
 							<Col>
-								<Form.Control placeholder="Last name" />
+								<Form.Group>
+									<Form.Label />
+									<span style={{ color: "red" }}>[Upload only JPG/JPEG]</span>
+									<Form.Control type="file" />
+								</Form.Group>
 							</Col>
 						</Row>
+						<Form>
+							<Form.Label>
+								Gender
+								<span style={{ color: "red" }}>*</span> :
+							</Form.Label>
+							<br />
+							<Form.Check.Input type={"radio"} isValid className="mb-3" />
+							<Form.Check.Label>Male</Form.Check.Label>
+							<br />
+							<Form.Check.Input type={"radio"} isValid className="mb-3" />
+							<Form.Check.Label>Female</Form.Check.Label>
+							<br />
+							<Form.Check.Input type={"radio"} isValid className="mb-3" />
+							<Form.Check.Label>Transgender</Form.Check.Label>
+							<br />
+						</Form>
+						<hr />
+						<Form>
+							<Form.Label column="lg" lg={2}>
+								Address Details
+								<span style={{ color: "red" }}>*</span>
+							</Form.Label>
+							<Form.Control
+								type="text"
+								placeholder="Address Line 1"
+								className="mb-3"
+							/>
+							<Form.Control
+								type="text"
+								placeholder="Address Line 2"
+								className="mb-3"
+							/>
+							<Row className="mb-3">
+								<Col>
+									<Form.Control type="text" placeholder="City" />
+								</Col>
+								<Col>
+									<Form.Control type="text" placeholder="Pin Code" />
+								</Col>
+							</Row>
+							<Row className="mb-3">
+								<Col>
+									<Form.Control type="text" placeholder="State" />
+								</Col>
+								<Col>
+									<Form.Control type="text" placeholder="INDIA" />
+								</Col>
+							</Row>
+						</Form>
+					</Card.Body>
+				</Card>
+
+				<Card>
+					<CardHeader>
+						Upload Applicant Signature <br />
+						<span style={{ color: "red" }}>[Upload only JPG/JPEG]</span>
+					</CardHeader>
+					<Card.Body>
+						<Form.Control type="file" />
+					</Card.Body>
+				</Card>
+
+				<Card>
+					<CardHeader>Declaration</CardHeader>
+					<Card.Body>
+						<Form.Check.Input type={"radio"} isValid className="mb-3" />
+						<Form.Check.Label>
+							I declare that all information provided by me in the application
+							is true to the best of my knowledge and belief. I understand that
+							i am liable for prosecution if any of the information is found to
+							be false at any time in future,
+						</Form.Check.Label>
 					</Card.Body>
 				</Card>
 			</div>
+
+			<Button variant="outline-success">Submit Application</Button>
+			<span style={{ color: "red" }}>CLICK ONLY ONCE</span>
+
+			<Navbar bg="dark" expand="md" variant="dark">
+				<Container fluid>
+					<Navbar.Brand href="#">CHARUSAT - PhD Admission Portal</Navbar.Brand>
+					<Button variant="outline-success">Logout</Button>
+				</Container>
+			</Navbar>
 		</>
 	);
 };

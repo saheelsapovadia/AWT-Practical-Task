47bc1e3199b60b7b7cc42aa26a862b54efe85651
Jaipal Jadeja <jadejajaipal5@gmail.com>
2021-11-13 10:14:34 +0530

    a few fixes

--- README.md
+++ README.md
@@ -9,9 +9,11 @@ First open two terminals in root directory
 In Terminal 1:
 
 1. run `cd server`
-2. run `npm start`
+2. run `npm i`
+3. run `npm start`
 
 In Terminal 2:
 
 1. run `cd client`
-2. run `npm start`
+2. run `npm i`
+3. run `npm start`
--- client/src/App.js
+++ client/src/App.js
@@ -2,21 +2,23 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./Home/Home";
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Display from "./Display/Display";
 
 function App() {
   return (
     <Router>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route exact path="/id/:id">
-        <Display />
-      </Route>
-      <Route exact path="*">
-        <h1>Such Page Doesnt Exist</h1>
-      </Route>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/id/:id">
+          <Display />
+        </Route>
+        <Route path="/*">
+          <h1>Such Page Doesnt Exist</h1>
+        </Route>
+      </Switch>
     </Router>
   );
 }
